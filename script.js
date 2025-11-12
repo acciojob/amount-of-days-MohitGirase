e415ac719dd3558150ee62cc5a41284a296f29e6
Mohit Girase <114944481+MohitGirase@users.noreply.github.com>
2025-11-12 19:35:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function daysOfAYear(year){
+	let flag = false;
+	if(year % 4 == 0){
+		if(year % 100 == 0){
+			if(year % 400 == 0){
+				flag = true;
+			}
+		}else{
+			flag = true;
+		}
+	}
+	return flag ? 366 : 365;
+}
